11890f728631e1424a9dd19d3dc7d5f393db8059
amityadav13a-prog! <you@example.com>
2026-02-16 23:14:14 +0530

    commitnull

---
+++ dsa/basics/tempCodeRunnerFile.cpp
@@ -0,0 +1,21 @@
+  #include <iostream>
+using namespace std;
+void printp(int n){
+    for(int i=0;i<n;i++)
+   {
+    for(int j=0;j<=(i);j++)
+     {cout<<" ";
+    }for(int j=0;j<(2*n-(2*i+1));j++){cout<<"*";}
+    for(int j=0;j<=(i);j++)
+     {cout<<" ";
+    } 
+    cout<<"\n";
+   }
+}
+int main(){int t;
+    cin>>t;
+for(int i=0;i<t;i++){
+    int n;
+    cin>>n;
+    printp(n);}
+    return 0;}
