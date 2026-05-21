d6e9809df0c0b849092bde492d788c00c25f2e16
amityadav13a-prog! <amityadav13a@gmail.com>
2026-05-21 21:48:00 +0530

    commitd1.1

--- dsafull/d1.cpp
+++ dsafull/d1.cpp
@@ -10,7 +10,7 @@
 // std::cout<<lr<<"\n";
 // std::cout<<"hey amit"<<std::endl;
 // return 0;}
-//                      second largest 
+//                                   we have to find                          second largest 
 // #include <iostream>
 // using namespace std;
 // int main(){
@@ -28,13 +28,39 @@
 // return 0;}
 //optimal in dono loop ko ek sathn run kr de optimal mil jayega
 //                 check if arr sorted
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int arr[5]={1,2,6,4,5};
+//  for(int i =0;i<=5;i++){
+//     if (arr[i]>arr[i+1])
+//     return false;
+//     }return  true;
+//     return 0;}
+//     //
+    #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int arr[5]={1,2,6,4,5};
- for(int i =0;i<=5;i++){
-    if (arr[i]>arr[i+1])
-    return false;
-    }return  true;
 
-    return 0;}
+// Function to check if the array is sorted
+bool isSorted(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            // If any element is smaller than the previous one, return false
+            if (arr[j] < arr[i]) 
+                return false;
+        }
+    }
+
+    return true; // Return true if no unsorted elements are found
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5}, n = 5;
+    bool ans = isSorted(arr, n);
+    
+    // Output result
+    if (ans) cout << "True" << endl;
+    else cout << "False" << endl;
+    
+    return 0;
+}
