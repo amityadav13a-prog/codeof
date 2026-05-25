03a2c6288a588c552af7ecd3d124ab255711d26b
amityadav13a-prog! <amityadav13a@gmail.com>
2026-05-25 22:18:37 +0530

    coomitdb

--- dsa/basics/pattern1.cpp
+++ dsa/basics/pattern1.cpp
@@ -10,6 +10,8 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
+
+    
    for(int i =0;i<n;i++)//for num of line,,, outer loop
    {
     for(int j=0;j<n;j++)//focus on colounm& then conect them
