e0e2f07a03735d65f98e60e84e04db07001aa60d
amityadav13a-prog! <amityadav13a@gmail.com>
2026-05-26 21:55:26 +0530

    commitd4

---
+++ dsafull/d4.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+class ListNode{
+public:
+    int val;
+    ListNode* next;
+
+    ListNode(int x){
+        val = x;
+        next = NULL;
+    }
+};
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2){
+
+    ListNode* head = new ListNode(0);
+    ListNode* temp = head;
+
+    int carry = 0;
+
+    while(l1 != NULL || l2 != NULL || carry){
+
+        int sum = carry;
+
+        if(l1 != NULL){
+            sum = sum + l1->val;
+            l1 = l1->next;
+        }
+
+        if(l2 != NULL){
+            sum = sum + l2->val;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+
+        temp->next = new ListNode(sum % 10);
+
+        temp = temp->next;
+    }
+
+    return head->next;
+}
+
+void print(ListNode* head){
+
+    while(head != NULL){
+        cout << head->val << " ";
+        head = head->next;
+    }
+}
+
+int main(){
+
+    // l1 = 2 4 3
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    // l2 = 5 6 4
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    ListNode* ans = addTwoNumbers(l1, l2);
+
+    print(ans);
+
+    return 0;
+}
