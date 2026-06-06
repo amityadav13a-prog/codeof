cb28316e302650604af1407af1c96a6b92611cfd
amityadav13a-prog! <amityadav13a@gmail.com>
2026-06-06 21:31:06 +0530

    mediancommit

--- dsafull/d7.cpp
+++ dsafull/d7.cpp
@@ -19,3 +19,37 @@ public:
         return rev;
     }
 };
+
+// mediadn of arry
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+        int m = nums1.size();
+        int n = nums2.size();
+
+        vector<int> v(m + n);
+
+        int i = 0, j = 0, k = 0;
+
+        while(i < m && j < n) {
+            if(nums1[i] <= nums2[j])
+                v[k++] = nums1[i++];
+            else
+                v[k++] = nums2[j++];
+        }
+
+        while(i < m)
+            v[k++] = nums1[i++];
+
+        while(j < n)
+            v[k++] = nums2[j++];
+
+        int size = m + n;
+
+        if(size % 2 == 0)
+            return (v[size/2] + v[size/2 - 1]) / 2.0;
+
+        return v[size/2];
+    }
+};
