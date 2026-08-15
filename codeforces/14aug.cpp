d82e66537eaa0f2aa9797e1b9a5bb91024289d0f
amityadav13a-prog! <amityadav13a@gmail.com>
2026-08-15 12:27:02 +0530

    14aug

---
+++ codeforces/14aug.cpp
@@ -0,0 +1,48 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int  solve() {
+// int n,k;
+// string ans="NO\n";
+// cin>>n;cin>>k;
+// int arr[n];
+// for(int i=0;i<n;i++){
+//     cin>>arr[i];
+// }
+// for(int i=0;i<n;i++){
+//     if(arr[i]==k){ans="YES\n";
+//     break;}
+// } cout<<ans;
+// }
+
+// int main(){
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//  int t;
+// cin >> t;
+// while(t>0){
+//     solve();
+//     t--;
+// }}
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int  solve() {
+// int n;
+// cin>>n;
+// int sum=0;
+// for(int i=0;i<(n-1);i++){
+//     int x;
+// cin>>x;
+// sum+=x;}
+//      cout<<(-1*sum)<<"\n";
+// }
+// int main(){
+//     // ios_base::sync_with_stdio(false);
+//     // cin.tie(NULL);
+//  int t;
+// cin >> t;
+// while(t--){
+//     solve();
+// }}
